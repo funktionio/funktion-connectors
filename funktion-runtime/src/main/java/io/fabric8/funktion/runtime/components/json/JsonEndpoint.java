192a783573ae5dd5a7371e9c0e6bdbc84adddb4d
Claus Ibsen <claus.ibsen@gmail.com>
2017-03-16 15:48:52 +0100

    Polished

--- funktion-runtime/src/main/java/io/fabric8/funktion/runtime/components/json/JsonEndpoint.java
+++ funktion-runtime/src/main/java/io/fabric8/funktion/runtime/components/json/JsonEndpoint.java
@@ -1,18 +1,18 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+/*
+ * Copyright 2016 Red Hat, Inc.
+ * <p>
+ * Red Hat licenses this file to you under the Apache License, version
+ * 2.0 (the "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
  * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
  */
 package io.fabric8.funktion.runtime.components.json;
 
@@ -35,8 +35,6 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
-/**
- */
 public class JsonEndpoint extends DefaultEndpoint {
 
     public static final String JSON_CONTENT_TYPE = "application/json";
@@ -129,9 +127,9 @@ public class JsonEndpoint extends DefaultEndpoint {
 
     private boolean isJsonLookingString(String possibleJson) {
         return (possibleJson.startsWith("{") && possibleJson.endsWith("}")) ||
-                (possibleJson.startsWith("[") && possibleJson.endsWith("]")) ||
-                (possibleJson.startsWith("\"") && possibleJson.endsWith("\"")) ||
-                (possibleJson.startsWith("'") && possibleJson.endsWith("'"));
+               (possibleJson.startsWith("[") && possibleJson.endsWith("]")) ||
+               (possibleJson.startsWith("\"") && possibleJson.endsWith("\"")) ||
+               (possibleJson.startsWith("'") && possibleJson.endsWith("'"));
     }
 
     /**
